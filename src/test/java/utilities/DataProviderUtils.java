29e03d827c8e4e74a0e90baf52a347d02358d609
Salih Aydin <salihtalha19991@gmail.com>
2021-11-22 11:13:43 -0600

    Data provider utils added

---
+++ src/test/java/utilities/DataProviderUtils.java
@@ -0,0 +1,12 @@
+package utilities;
+
+import org.testng.annotations.DataProvider;
+
+public class DataProviderUtils {
+
+    @DataProvider(name = "data from exampleExcel")
+    public static Object[][] getExampleExcelData(){
+        ExcelUtils.openExcelFile("Sheet1");
+        return ExcelUtils.convert2DListToObject(ExcelUtils.getValues());
+    }
+}
