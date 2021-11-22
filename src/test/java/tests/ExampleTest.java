e8545af3147ba51aef90ef593a5373995464f56d
Salih Aydin <salihtalha19991@gmail.com>
2021-11-22 10:50:37 -0600

    ...

--- src/test/java/tests/ExampleTest.java
+++ src/test/java/tests/ExampleTest.java
@@ -1,7 +1,6 @@
 package tests;
 
 import org.testng.annotations.Test;
-import utilities.Hooks;
 
 public class ExampleTest extends Hooks {
 
--- src/test/java/tests/Hooks.java
+++ src/test/java/tests/Hooks.java
@@ -1,8 +1,9 @@
-package utilities;
+package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import utilities.Driver;
 
 public class Hooks {
 
